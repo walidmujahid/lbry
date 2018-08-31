7d390749dccf4ca3d3dbb15b1fc1838474acb9ba
hackrush <atulshyan96@gmail.com>
2018-08-31 23:31:50 +0530

    Review Fixes

--- tests/integration/wallet/test_commands.py
+++ tests/integration/wallet/test_commands.py
@@ -12,7 +12,6 @@ lbryschema.BLOCKCHAIN_NAME = 'lbrycrd_regtest'
 
 from torba.constants import COIN
 
-
 from lbrynet import conf as lbry_conf
 from lbrynet.dht.node import Node
 from lbrynet.daemon.Daemon import Daemon
@@ -89,7 +88,7 @@ class FakeAnalytics:
 
 class CommandTestCase(IntegrationTestCase):
 
-    timeout = 300
+    timeout = 180
     WALLET_MANAGER = LbryWalletManager
 
     async def setUp(self):
